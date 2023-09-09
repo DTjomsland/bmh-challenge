08f1d4bb0067ec54c0b25a22bc5812fb9b9af052
David Tjomsland <dtjomslandjr@gmail.com>
2023-09-09 13:25:17 +0800

    added safari compatibility

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,6 +8,7 @@ import { ResizeListenerDirective } from './resize-listener.directive';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ModelComponent } from './model/model.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 const routes: Routes = [
   { path: 'pie-chart', component: PieChartComponent }, // Define routes for your components
@@ -27,7 +28,13 @@ const routes: Routes = [
     AppRoutingModule,
     HttpClientModule,
     NgbModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   exports: [RouterModule],
   providers: [],
